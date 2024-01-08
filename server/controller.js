bdc11a580f45a7d24bfa902dc4b7ec67aa7f49c5
Moriya Katan <moriyakatan@gmail.com>
2024-01-08 15:01:49 +0200

    updated APIFeatures

--- server/controller.js
+++ server/controller.js
@@ -1,70 +1,26 @@
-const fs = require('fs');
+//const fs = require('fs');
 const Customer = require('./model');
+const APIFeatures = require('./utils/apiFeatures')
 
 exports.customerName = (req, res, next) => {
   req.query.fields = 'name, phone';
   next();
-}
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-  
-}
+};
 
 exports.getAllCustomers = async (req, res) => {
   try {
-    console.log(req.query);
-    //Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-    //console.log(req.query, queryObj)
-
-    //Advanced filter
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    //console.log(JSON.parse(queryStr));
-    let query = Customer.find(JSON.parse(queryStr));
-
-    //Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    //Fieldsd
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    //Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    //page=3&limit=10, 1-10, page=1, 1-20, page=2, 21-30, page=3
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numCustomers = await Customer.countDocuments();
-      if(skip >= numCustomers) throw new Error('This page does not exist')
-    }
-
     //Execute Query
-    const customers = await query;
+    const features = new APIFeatures(Customer.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const customers = await features.query;
     // query.sort().select().skip().limit()
 
     // Send response
     res.status(200).json({
       status: 'success',
-      //requestedAt: req.requestTime,
       results: customers.length,
       data: {
         customers,
